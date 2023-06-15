27c1f48cb1973c0491cb7a40c3f22c40c6ff1df5
Dell <Dell@DESKTOP-CJLCDUR>
2023-06-15 23:18:19 +0530

    qno1:added array addition

---
+++ MavenJavaPrograms/src/main/java/ObsquraPrograms/ArrayAddition1.java
@@ -0,0 +1,20 @@
+package ObsquraPrograms;
+
+public class ArrayAddition1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int s = 0;
+		int a[] = {1,2,3,4};
+		/*int l = Integer.parseInt(args[0]);
+		int m = Integer.parseInt(args[1]);
+		int n = Integer.parseInt(args[2]);
+		int o = Integer.parseInt(args[3]);*/
+		for (int i = 0; i <= 3; i++) {
+			s = s+a[i];
+		}
+		System.out.println(s);
+	}
+
+}
